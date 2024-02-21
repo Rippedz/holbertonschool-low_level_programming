ecba8d82089f5a7a1af3d0ec1500b85c7f7ebbe4
Rippedz <ttfoot12@gmail.com>
2024-02-21 13:04:01 +0000

    variable

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Base16'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = '0';
+
+	while ( x <= '9')
+	{
+		putchar(x);
+		x++;
+	};
+
+		x = 'a';
+
+	while ( x <= 'f')
+	{
+		putchar(x);
+		x++;
+	};
+
+	putchar('\n');
+
+	return (0);
+}
