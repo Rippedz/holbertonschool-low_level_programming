ef9c8daef6924d63d111fc79fd34883b2c784998
Rippedz <ttfoot12@gmail.com>
2024-03-04 15:54:20 +0100

    hi

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strpbrk - gets the length of a prefix substring                     * @s: character to be print                                            * @accept: string containing the characters to match                  *                                                                     * Return: return always 0
- * ibytes from accept
- */
-char *_strpbrk(char *s, char *accept)
-{
-	int i, j;
+ 
+_strpbrk - Entry point
+@accept: character to print
+@s: character to print
+*
+Return: Nothing.
+*/
 
-	for (i = 0 ; s[i]  != '\0' ; i++)
-	{
-		for (j = 0 ; accept[j] != '\0' ; j++)
-		{	
-			if (s[i] == accept[j])
-			{	
-				return (&s[i]);
-			}
-		}
-	}	
-return('\0');
+char _strpbrk(chars, char *accept)
+{
+        int i, j;
+
+        for (i = 0 ; s[i] != '\0' ; i++)
+        {
+                for (j = 0 ; accept[j] != '\0' ; j++)
+                {
+                        if (s[i] == accept[j])
+                        {
+                                return (&s[i]);
+                        }
+                }
+        }
+        return ('\0');
 }
