53d14db01c3f142755350a07d84233df245f310b
Rippedz <ttfoot12@gmail.com>
2024-03-06 02:45:02 +0100

    hi

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,17 @@
  */
 void _puts_recursion(char *s)
 {
+	/* if string (s) pointer is char (NULL) */
+	/* return (void) */
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 
+	/* call _puts_recursion function with */
+	/* incremented pointer (s + 1) */
+	/* to print the next character in the string */
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
